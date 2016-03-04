e63d7886278394438994753a261d433b8d859cfe
Scott Murphy <scottm@mtolympus.partners.directs.com>
2016-03-04 11:13:50 -0600

    first commit

---
+++ client.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function () {
+	var deadForm = $("#deadForm")[0];
+	var deadForm_name = $("#name")[0];
+	var deadForm_color = $("#color")[0];
+	var deadForm_play = $("#play")[0];
+
+	var canvas = $("#canvas")[0];
+	var ctx = canvas.getContext("2d");
+	var screen_width = $("#canvas").width();
+	var screen_height = $("#canvas").height();
+	var cell_width = 10;
+
+	var snakes = {};
+	var food = {};
+	var me = { alive: 0 };
+
+	var socket = io.connect();
+
+	function generateId() {
+		return Date.now();
+	}
+
+	function paintCell(x, y, color) {
+		ctx.fillStyle = color;
+		ctx.fillRect(x * cell_width, y * cell_width, cell_width, cell_width);
+		ctx.strokeStyle = "white";
+		ctx.strokeRect(x * cell_width, y * cell_width, cell_width, cell_width);
+	}
+
+	function paintSnakes() {
+		for (snakeId in snakes) {
+			var snake = snakes[snakeId];
+			for (var i = 0; i < snake.cells.length; i++) {
+				var cell = snake.cells[i];
+				paintCell(cell.x, cell.y, snake.color);
+			}
+		}
+	}
+
+	function paintFood() {
+		for (foodId in food) {
+			var eatMe = food[foodId];
+			paintCell(eatMe.x, eatMe.y, "Green");
+		}
+	}
+
+	function paintBackground() {
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, screen_width, screen_height);
+		ctx.strokeStyle = "black";
+		ctx.strokeRect(0, 0, screen_width, screen_height);
+	}
+
+	setInterval(function () {
+		paintBackground();
+		paintSnakes();
+		paintFood();
+
+		if (me.alive === 0) {
+			deadForm.style.display = "block";
+		}
+	}, 10);
+
+	deadForm_play.onclick = function (evt) {
+		var name = deadForm_name.value;
+		var color = deadForm_color.value;
+
+		if (name.length < 3 || name.length > 20) {
+			alert('Name must be between 3 - 20 characters.');
+		} else {
+			me = {
+				id: generateId(),
+				name: name,
+				color: color,
+				direction: 'right',
+				alive: 1
+			};
+
+			socket.emit('snake', me);
+			deadForm.style.display = "none";
+		}
+	};
+
+	socket.on('snakes', function (data) {
+		snakes = data;
+	});
+
+	socket.on('food', function (data) {
+		food = data;
+	});
+
+	socket.on('killedSnakes', function (data) {
+		for (id in data) {
+			if (id == me.id) {
+				me.alive = 0;
+			}
+		}
+	});
+
+	$(document).keydown(function (e) {
+		if (me.alive === 1) {
+			var key = e.which;
+			var newDir = me.direction;
+
+			if (key == "37" && me.direction !== "right" && me.direction !== "left") newDir = "left";
+			else if (key == "38" && me.direction !== "down" && me.direction !== "up") newDir = "up";
+			else if (key == "39" && me.direction !== "left" && me.direction !== "right") newDir = "right";
+			else if (key == "40" && me.direction !== "up" && me.direction !== "down") newDir = "down";
+
+			if (me.direction !== newDir) {
+				me.direction = newDir;
+				socket.emit('direction', {
+					id: me.id,
+					direction: me.direction
+				});
+			}
+		}
+	});
+});
---
+++ server.js
@@ -0,0 +1,153 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client.js', function (req, res) {
+	res.sendFile(__dirname + '/client.js');
+});
+
+http.listen(8080, function () { });
+
+var snakes = {};
+var killedSnakes = {};
+var food = {};
+var max_food = 5;
+var grid_max_width = 100;
+var grid_max_height = 70;
+var starting_snake_length = 5;
+
+function log(message) {
+	console.log(Date.now() + ': ' + message);
+}
+
+io.on('connection', function (socket) {
+	socket.on('snake', function (data) {
+		var newSnake = {
+			id: data.id,
+			name: data.name,
+			color: data.color,
+			direction: 'right',
+			cells: []
+		};
+
+		var startingLocation = randomCoordinates();
+		while (startingLocation.x < starting_snake_length) {
+			startingLocation = randomCoordinates();
+		}
+		
+		for (var i = 0; i < starting_snake_length; i++) {
+			newSnake.cells.push({ x: startingLocation.x - i, y: startingLocation.y });
+		}
+
+		snakes[newSnake.id] = newSnake;
+	});
+
+	socket.on('direction', function (data) {
+		var snake = snakes[data.id];
+		snake.direction = data.direction;
+	});
+
+	setInterval(function () {
+		socket.emit('snakes', snakes);
+		socket.emit('food', food);
+		socket.emit('killedSnakes', killedSnakes);
+	}, 10);
+});
+
+setInterval(function () {
+	for (id in snakes) {
+		var snake = snakes[id];
+
+		var newX = snake.cells[0].x;
+		var newY = snake.cells[0].y;
+		if (snake.direction === 'right') {
+			newX++;
+		} else if (snake.direction === 'left') {
+			newX--;
+		} else if (snake.direction === 'down') {
+			newY++;
+		} else if (snake.direction === 'up') {
+			newY--;
+		}
+
+		if (newX === -1 || newX === grid_max_width || newY === -1 || newY === grid_max_height || checkSnakeCollision(newX, newY)) {
+			killedSnakes[id] = id;
+			delete snakes[id];
+		} else {
+			var eatenFood = checkFoodCollision(newX, newY);
+			if (eatenFood >= 0) {
+				delete food[eatenFood];
+				createFood();
+			} else {
+				snake.cells.pop();
+			}
+
+			snake.cells.unshift({
+				x: newX,
+				y: newY
+			});
+		}
+	}
+}, 60);
+
+setInterval(function () {
+	createFood();
+	killedSnakes = {};
+}, 15000);
+
+function randomCoordinates() {
+	return {
+		x: Math.round(Math.random() * grid_max_width),
+		y: Math.round(Math.random() * grid_max_height)
+	};
+}
+
+function checkSnakeCollision(x, y, id) {
+	for (snakeId in snakes) {
+		var snake = snakes[snakeId];
+		if (snake.id !== id) {
+			for (var i = 0; i < snake.cells.length; i++) {
+				if (snake.cells[i].x === x && snake.cells[i].y === y) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function checkFoodCollision(x, y) {
+	for (id in food) {
+		if (x === food[id].x && y === food[id].y) {
+			return id;
+		}
+	}
+	return -1;
+}
+
+function countSnakes() {
+	var count = 0;
+	for (snake in snakes) {
+		count++;
+	}
+	return count;
+}
+
+function coundFood() {
+	var count = 0;
+	for (f in food) {
+		count++;
+	}
+	return count;
+}
+
+function createFood() {
+	if (coundFood() < Math.min(Math.max(1, countSnakes()), max_food)) {
+		food[Date.now()] = randomCoordinates();
+	}
+}
+createFood();
